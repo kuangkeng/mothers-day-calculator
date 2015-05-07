c98b9e16365af0f3fdb322cb2829790b04ba6e15
Kuang Keng Kuek Ser <kuangkeng@gmail.com>
2015-05-07 14:24:52 -0400

    third version changed

--- script3.js
+++ script3.js
@@ -3,157 +3,99 @@ $("#result, #bottomform, #occupation, #bls").hide();
 var data = {
 			  cooking: 20.01,
 			  driving: 11.16,
-			  homeworkpre: 16.39,
-			  homeworksec: 14.62,
+			  homework: 14.62,
 			  kids: 9.48,
 			  cleaning: 10.98,
 			  planning: 22.35,
 			  shopping: 9.83,
 			  finances: 31.7,
 			  yard: 11.68,
-			  decorating: 23.27,
 			  finding: 21.43,
-			  counseling: 21.4
 			};
 
 $(document).ready(function(){
 
 //form validation using jquery validation plugin
 	 $("#mothersform").validate({
-		rules: {
-					cookinghour: {
-						required: true,
-						range: [0,84]
-					},
-					
-					drivinghour: {
-						required: true,
-						range: [0,84]
-					},
-					homeworkhour: {
-						required: true,
-						range: [0,84]
-					},
-					kidshour: {
-						required: true,
-						range: [0,84]
-					},
-					cleaninghour: {
-						required: true,
-						range: [0,84]
-					},
-					planninghour: {
-						required: true,
-						range: [0,84]
-					},
-					shoppinghour: {
-						required: true,
-						range: [0,84]
-					},
-					financeshour: {
-						required: true,
-						range: [0,84]
-					},
-					yardhour: {
-						required: true,
-						range: [0,84]
-					},
-					decoratinghour: {
-						required: true,
-						range: [0,84]
-					},
-					findinghour: {
-						required: true,
-						range: [0,84]
-					},
-					counselinghour: {
-						required: true,
-						range: [0,84]
-					},
-				},
-//messages to be displayed if input cannot be validated
-		messages: {
-                    cookinghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    drivinghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    homeworkhour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    kidshour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    cleaninghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    planninghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    shoppinghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    financeshour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    yardhour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    decoratinghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    findinghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                    counselinghour: {
-                        required: "Please answer this question",
-                        range: "Must be a number between 0 and 84"
-                    },
-                },
-
-//display error messages style if input cannot be validated
-       errorPlacement: function(error, element) {
- //       error.insertAfter(element);
-        offset = element.offset();
-            error.insertAfter(element)
-            error.addClass('errormessage');  // add a class to the wrapper
-   			error.css('position', 'absolute');
-            error.css('left', offset.left);
-            error.css('top', offset.top + element.outerHeight() + 5);
-    },
-    wrapper: 'div',   
-
+		
 submitHandler: function(form) {
 
-//setting the input variables
-		var inputcooking = ($('#inputcookinghour').val()*data.cooking),
-			inputdriving = ($('#inputdrivinghour').val()*data.driving),
-			inputhomework = ($('#inputhomeworkhour').val()*data.homeworksec),
-			inputkids = ($('#inputkidshour').val()*data.kids),
-			inputcleaning = ($('#inputcleaninghour').val()*data.cleaning),
-			inputplanning = ($('#inputplanninghour').val()*data.planning),
-			inputshopping = ($('#inputshoppinghour').val()*data.shopping),
-			inputfinances = ($('#inputfinanceshour').val()*data.finances),
-			inputyard = ($('#inputyardhour').val()*data.yard),
-			inputdecorating = ($('#inputdecoratinghour').val()*data.decorating),
-			inputfinding = ($('#inputfindinghour').val()*data.finding),
-			inputcounseling = ($('#inputcounselinghour').val()*data.counseling);
+//grab the input
+		var inputcooking = $('#inputcookinghour').val(),
+			inputdriving = $('#inputdrivinghour').val(),
+			inputhomework = $('#inputhomeworkhour').val(),
+			inputkids = $('#inputkidshour').val(),
+			inputcleaning = $('#inputcleaninghour').val(),
+			inputplanning = $('#inputplanninghour').val(),
+			inputshopping = $('#inputshoppinghour').val(),
+			inputfinances = $('#inputfinanceshour').val(),
+			inputyard = $('#inputyardhour').val(),
+			inputdecorating = $('#inputdecoratinghour').val(),
+			inputfinding = $('#inputfindinghour').val(),
+			inputcounseling = $('#inputcounselinghour').val();
 		
-		var totalwage = 52*(inputcooking + inputdriving + inputhomework + inputkids + inputcleaning + inputplanning + inputshopping + inputfinances + inputyard + inputdecorating + inputfinding + inputcounseling),
+//convert the input into hours
+		if(inputcooking=="Never"){var cookinghour=0;}
+		if(inputcooking=="Seldom"){var cookinghour=1;}
+		if(inputcooking=="Always"){var cookinghour=5;}
+		if(inputcooking=="All the time!"){var cookinghour=14;}
+
+		if(inputdriving=="Never"){var drivinghour=0;}
+		if(inputdriving=="Seldom"){var drivinghour=1;}
+		if(inputdriving=="Always"){var drivinghour=5;}
+		if(inputdriving=="All the time!"){var drivinghour=14;}
+
+		if(inputhomework=="Never"){var homeworkhour=0;}
+		if(inputhomework=="Seldom"){var homeworkhour=1;}
+		if(inputhomework=="Always"){var homeworkhour=5;}
+		if(inputhomework=="All the time!"){var homeworkhour=14;}
+
+		if(inputcleaning=="Never"){var cleaninghour=0;}
+		if(inputcleaning=="Seldom"){var cleaninghour=1;}
+		if(inputcleaning=="Always"){var cleaninghour=5;}
+		if(inputcleaning=="All the time!"){var cleaninghour=14;}
+
+		if(inputplanning=="Never"){var planninghour=0;}
+		if(inputplanning=="Seldom"){var planninghour=1;}
+		if(inputplanning=="Always"){var planninghour=5;}
+		if(inputplanning=="All the time!"){var planninghour=14;}
+
+		if(inputshopping=="Never"){var shoppinghour=0;}
+		if(inputshopping=="Seldom"){var shoppinghour=1;}
+		if(inputshopping=="Always"){var shoppinghour=5;}
+		if(inputshopping=="All the time!"){var shoppinghour=14;}
+
+		if(inputfinances=="Never"){var financeshour=0;}
+		if(inputfinances=="Seldom"){var financeshour=1;}
+		if(inputfinances=="Always"){var financeshour=5;}
+		if(inputfinances=="All the time!"){var financeshour=14;}
+
+		if(inputfinding=="Never"){var findinghour=0;}
+		if(inputfinding=="Seldom"){var findinghour=1;}
+		if(inputfinding=="Always"){var findinghour=5;}
+		if(inputfinding=="All the time!"){var findinghour=14;}
+
+		if(inputyard=="Never"){var yardhour=0;}
+		if(inputyard=="Seldom"){var yardhour=1;}
+		if(inputyard=="Always"){var yardhour=5;}
+		if(inputyard=="All the time!"){var yardhour=14;}
+
+//convert hours into wage
+		var cookingwage = cookinghour*data.cooking
+			drivingwage  = drivinghour*data.driving
+			homeworkwage = homeworkhour*data.homework
+			cleaningwage = cleaninghour*data.cleaning
+			planningwage = planninghour*data.planning
+			shoppingwage = shoppinghour*data.shopping
+			financeswage = financeshour*data.finances
+			findingwage = findinghour*data.finding
+			yardwage = yardhour*data.yard
+
+//add up the total wage
+		var totalwage = 52*(cookingwage + drivingwage + homeworkwage + kidswage + cleaningwage + planningwage + shoppingwage + financeswage + yardwage + findingwage),
 			totalwageround = totalwage.toFixed(2);
 
+//verify the variables
 console.log("input = " + $('#inputcookinghour').val());
 console.log("input = " + $('#inputdrivinghour').val());
 console.log("input = " + $('#inputhomeworkhour').val());
@@ -163,9 +105,7 @@ console.log("input = " + $('#inputplanninghour').val());
 console.log("input = " + $('#inputshoppinghour').val());
 console.log("input = " + $('#inputfinanceshour').val());
 console.log("input = " + $('#inputyardhour').val());
-console.log("input = " + $('#inputdecoratinghour').val());
 console.log("input = " + $('#inputfindinghour').val());
-console.log("input = " + $('#inputcounselinghour').val());
 
 console.log("data = " + data.cooking);
 console.log("data = " + data.driving);
@@ -176,23 +116,30 @@ console.log("data = " + data.planning);
 console.log("data = " + data.shopping);
 console.log("data = " + data.finances);
 console.log("data = " + data.yard);
-console.log("data = " + data.decorating);
 console.log("data = " + data.finding);
-console.log("data = " + data.counseling);
 
-console.log("sum = " + inputcooking);
-console.log("sum = " + inputdriving);
-console.log("sum = " + inputhomework);
-console.log("sum = " + inputkids);
-console.log("sum = " + inputcleaning);
-console.log("sum = " + inputplanning);
-console.log("sum = " + inputshopping);
-console.log("sum = " + inputfinances);
-console.log("sum = " + inputyard);
-console.log("sum = " + inputdecorating);
-console.log("sum = " + inputfinding);
-console.log("sum = " + inputcounseling);
-				
+console.log("hour = " + cookinghour);
+console.log("hour = " + drivinghour);
+console.log("hour = " + homeworkhour);
+console.log("hour = " + kidshour);
+console.log("hour = " + cleaninghour);
+console.log("hour = " + planninghour);
+console.log("hour = " + shoppinghour);
+console.log("hour = " + financeshour);
+console.log("hour = " + yardhour);
+console.log("hour = " + findinghour);
+
+console.log("wage = " + cookingwage);
+console.log("wage = " + drivingwage);
+console.log("wage = " + homeworkwage);
+console.log("wage = " + kidswage);
+console.log("wage = " + cleaningwage);
+console.log("wage = " + planningwage);
+console.log("wage = " + shoppingwage);
+console.log("wage = " + financeswage);
+console.log("wage = " + yardwage);
+console.log("wage = " + findingwage);
+
 console.log("totalwage is: " + totalwage);
 console.log("totalwageround is: " + totalwageround);
 
@@ -201,8 +148,6 @@ $("#totalnumber").text(totalwageround);
 $("#occupation").text("general maneger");
 $("#button").hide();
 
-
     }
-
 
 })});
