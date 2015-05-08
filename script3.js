d3689759e0dda19fb0cc11a876188c760bf294de
Kuang Keng Kuek Ser <kuangkeng@gmail.com>
2015-05-08 09:18:37 -0400

    all tasks changed

--- script3.js
+++ script3.js
@@ -2,15 +2,15 @@ $("#result, #bottomform, #occupation, #td_bls, #td_blsrate").hide();
 
 var data = {
 			  task_1: 20.01,
-			  driving: 11.16,
-			  homework: 14.62,
-			  kids: 9.48,
-			  cleaning: 10.98,
-			  planning: 22.35,
-			  shopping: 9.83,
-			  finances: 31.7,
-			  yard: 11.68,
-			  finding: 21.43,
+			  task_2: 11.16,
+			  task_3: 14.62,
+			  task_4: 9.48,
+			  task_5: 10.98,
+			  task_6: 22.35,
+			  task_7: 9.83,
+			  task_8: 31.7,
+			  task_9: 11.68,
+			  task_10: 21.43,
 			};
 
 $(document).ready(function(){
@@ -22,15 +22,15 @@ submitHandler: function(form) {
 
 //grab the input
 		var inputtask_1 = $('#inputtask_1hour').val(),
-			inputdriving = $('#inputdrivinghour').val(),
-			inputhomework = $('#inputhomeworkhour').val(),
-			inputkids = $('#inputkidshour').val(),
-			inputcleaning = $('#inputcleaninghour').val(),
-			inputplanning = $('#inputplanninghour').val(),
-			inputshopping = $('#inputshoppinghour').val(),
-			inputfinances = $('#inputfinanceshour').val(),
-			inputyard = $('#inputyardhour').val(),
-			inputfinding = $('#inputfindinghour').val();
+			inputtask_2 = $('#inputtask_2hour').val(),
+			inputtask_3 = $('#inputtask_3hour').val(),
+			inputtask_4 = $('#inputtask_4hour').val(),
+			inputtask_5 = $('#inputtask_5hour').val(),
+			inputtask_6 = $('#inputtask_6hour').val(),
+			inputtask_7 = $('#inputtask_7hour').val(),
+			inputtask_8 = $('#inputtask_8hour').val(),
+			inputtask_9 = $('#inputtask_9hour').val(),
+			inputtask_10 = $('#inputtask_10hour').val();
 		
 //convert the input into hours
 		if(inputtask_1=="Never"){var task_1hour=0;}
@@ -38,126 +38,126 @@ submitHandler: function(form) {
 		if(inputtask_1=="Always"){var task_1hour=5;}
 		if(inputtask_1=="All the time!"){var task_1hour=14;}
 
-		if(inputdriving=="Never"){var drivinghour=0;}
-		if(inputdriving=="Seldom"){var drivinghour=1;}
-		if(inputdriving=="Always"){var drivinghour=5;}
-		if(inputdriving=="All the time!"){var drivinghour=14;}
+		if(inputtask_2=="Never"){var task_2hour=0;}
+		if(inputtask_2=="Seldom"){var task_2hour=1;}
+		if(inputtask_2=="Always"){var task_2hour=5;}
+		if(inputtask_2=="All the time!"){var task_2hour=14;}
 
-		if(inputhomework=="Never"){var homeworkhour=0;}
-		if(inputhomework=="Seldom"){var homeworkhour=1;}
-		if(inputhomework=="Always"){var homeworkhour=5;}
-		if(inputhomework=="All the time!"){var homeworkhour=14;}
+		if(inputtask_3=="Never"){var task_3hour=0;}
+		if(inputtask_3=="Seldom"){var task_3hour=1;}
+		if(inputtask_3=="Always"){var task_3hour=5;}
+		if(inputtask_3=="All the time!"){var task_3hour=14;}
 
-		if(inputkids=="Never"){var kidshour=0;}
-		if(inputkids=="Seldom"){var kidshour=1;}
-		if(inputkids=="Always"){var kidshour=5;}
-		if(inputkids=="All the time!"){var kidshour=14;}
+		if(inputtask_4=="Never"){var task_4hour=0;}
+		if(inputtask_4=="Seldom"){var task_4hour=1;}
+		if(inputtask_4=="Always"){var task_4hour=5;}
+		if(inputtask_4=="All the time!"){var task_4hour=14;}
 
-		if(inputcleaning=="Never"){var cleaninghour=0;}
-		if(inputcleaning=="Seldom"){var cleaninghour=1;}
-		if(inputcleaning=="Always"){var cleaninghour=5;}
-		if(inputcleaning=="All the time!"){var cleaninghour=14;}
+		if(inputtask_5=="Never"){var task_5hour=0;}
+		if(inputtask_5=="Seldom"){var task_5hour=1;}
+		if(inputtask_5=="Always"){var task_5hour=5;}
+		if(inputtask_5=="All the time!"){var task_5hour=14;}
 
-		if(inputplanning=="Never"){var planninghour=0;}
-		if(inputplanning=="Seldom"){var planninghour=1;}
-		if(inputplanning=="Always"){var planninghour=5;}
-		if(inputplanning=="All the time!"){var planninghour=14;}
+		if(inputtask_6=="Never"){var task_6hour=0;}
+		if(inputtask_6=="Seldom"){var task_6hour=1;}
+		if(inputtask_6=="Always"){var task_6hour=5;}
+		if(inputtask_6=="All the time!"){var task_6hour=14;}
 
-		if(inputshopping=="Never"){var shoppinghour=0;}
-		if(inputshopping=="Seldom"){var shoppinghour=1;}
-		if(inputshopping=="Always"){var shoppinghour=5;}
-		if(inputshopping=="All the time!"){var shoppinghour=14;}
+		if(inputtask_7=="Never"){var task_7hour=0;}
+		if(inputtask_7=="Seldom"){var task_7hour=1;}
+		if(inputtask_7=="Always"){var task_7hour=5;}
+		if(inputtask_7=="All the time!"){var task_7hour=14;}
 
-		if(inputfinances=="Never"){var financeshour=0;}
-		if(inputfinances=="Seldom"){var financeshour=1;}
-		if(inputfinances=="Always"){var financeshour=5;}
-		if(inputfinances=="All the time!"){var financeshour=14;}
+		if(inputtask_8=="Never"){var task_8hour=0;}
+		if(inputtask_8=="Seldom"){var task_8hour=1;}
+		if(inputtask_8=="Always"){var task_8hour=5;}
+		if(inputtask_8=="All the time!"){var task_8hour=14;}
 
-		if(inputfinding=="Never"){var findinghour=0;}
-		if(inputfinding=="Seldom"){var findinghour=1;}
-		if(inputfinding=="Always"){var findinghour=5;}
-		if(inputfinding=="All the time!"){var findinghour=14;}
+		if(inputtask_10=="Never"){var task_10hour=0;}
+		if(inputtask_10=="Seldom"){var task_10hour=1;}
+		if(inputtask_10=="Always"){var task_10hour=5;}
+		if(inputtask_10=="All the time!"){var task_10hour=14;}
 
-		if(inputyard=="Never"){var yardhour=0;}
-		if(inputyard=="Seldom"){var yardhour=1;}
-		if(inputyard=="Always"){var yardhour=5;}
-		if(inputyard=="All the time!"){var yardhour=14;}
+		if(inputtask_9=="Never"){var task_9hour=0;}
+		if(inputtask_9=="Seldom"){var task_9hour=1;}
+		if(inputtask_9=="Always"){var task_9hour=5;}
+		if(inputtask_9=="All the time!"){var task_9hour=14;}
 
 //convert hours into wage
 		var task_1wage = task_1hour*data.task_1
-			drivingwage  = drivinghour*data.driving
-			homeworkwage = homeworkhour*data.homework
-			kidswage = kidshour*data.kids
-			cleaningwage = cleaninghour*data.cleaning
-			planningwage = planninghour*data.planning
-			shoppingwage = shoppinghour*data.shopping
-			financeswage = financeshour*data.finances
-			findingwage = findinghour*data.finding
-			yardwage = yardhour*data.yard
+			task_2wage  = task_2hour*data.task_2
+			task_3wage = task_3hour*data.task_3
+			task_4wage = task_4hour*data.task_4
+			task_5wage = task_5hour*data.task_5
+			task_6wage = task_6hour*data.task_6
+			task_7wage = task_7hour*data.task_7
+			task_8wage = task_8hour*data.task_8
+			task_10wage = task_10hour*data.task_10
+			task_9wage = task_9hour*data.task_9
 
 //add up the total wage
-		var totalwage = 52*(task_1wage + drivingwage + homeworkwage + kidswage + cleaningwage + planningwage + shoppingwage + financeswage + yardwage + findingwage),
+		var totalwage = 52*(task_1wage + task_2wage + task_3wage + task_4wage + task_5wage + task_6wage + task_7wage + task_8wage + task_9wage + task_10wage),
 			totalwageround = totalwage.toFixed(2);
 
 //verify the variables
 console.log("input = " + $('#inputtask_1hour').val());
-console.log("input = " + $('#inputdrivinghour').val());
-console.log("input = " + $('#inputhomeworkhour').val());
-console.log("input = " + $('#inputkidshour').val());
-console.log("input = " + $('#inputcleaninghour').val());
-console.log("input = " + $('#inputplanninghour').val());	
-console.log("input = " + $('#inputshoppinghour').val());
-console.log("input = " + $('#inputfinanceshour').val());
-console.log("input = " + $('#inputyardhour').val());
-console.log("input = " + $('#inputfindinghour').val());
+console.log("input = " + $('#inputtask_2hour').val());
+console.log("input = " + $('#inputtask_3hour').val());
+console.log("input = " + $('#inputtask_4hour').val());
+console.log("input = " + $('#inputtask_5hour').val());
+console.log("input = " + $('#inputtask_6hour').val());	
+console.log("input = " + $('#inputtask_7hour').val());
+console.log("input = " + $('#inputtask_8hour').val());
+console.log("input = " + $('#inputtask_9hour').val());
+console.log("input = " + $('#inputtask_10hour').val());
 
 console.log("data = " + data.task_1);
-console.log("data = " + data.driving);
-console.log("data = " + data.homework);
-console.log("data = " + data.kids);
-console.log("data = " + data.cleaning);
-console.log("data = " + data.planning);
-console.log("data = " + data.shopping);
-console.log("data = " + data.finances);
-console.log("data = " + data.yard);
-console.log("data = " + data.finding);
+console.log("data = " + data.task_2);
+console.log("data = " + data.task_3);
+console.log("data = " + data.task_4);
+console.log("data = " + data.task_5);
+console.log("data = " + data.task_6);
+console.log("data = " + data.task_7);
+console.log("data = " + data.task_8);
+console.log("data = " + data.task_9);
+console.log("data = " + data.task_10);
 
 console.log("hour = " + task_1hour);
-console.log("hour = " + drivinghour);
-console.log("hour = " + homeworkhour);
-console.log("hour = " + kidshour);
-console.log("hour = " + cleaninghour);
-console.log("hour = " + planninghour);
-console.log("hour = " + shoppinghour);
-console.log("hour = " + financeshour);
-console.log("hour = " + yardhour);
-console.log("hour = " + findinghour);
+console.log("hour = " + task_2hour);
+console.log("hour = " + task_3hour);
+console.log("hour = " + task_4hour);
+console.log("hour = " + task_5hour);
+console.log("hour = " + task_6hour);
+console.log("hour = " + task_7hour);
+console.log("hour = " + task_8hour);
+console.log("hour = " + task_9hour);
+console.log("hour = " + task_10hour);
 
 console.log("wage = " + task_1wage);
-console.log("wage = " + drivingwage);
-console.log("wage = " + homeworkwage);
-console.log("wage = " + kidswage);
-console.log("wage = " + cleaningwage);
-console.log("wage = " + planningwage);
-console.log("wage = " + shoppingwage);
-console.log("wage = " + financeswage);
-console.log("wage = " + yardwage);
-console.log("wage = " + findingwage);
+console.log("wage = " + task_2wage);
+console.log("wage = " + task_3wage);
+console.log("wage = " + task_4wage);
+console.log("wage = " + task_5wage);
+console.log("wage = " + task_6wage);
+console.log("wage = " + task_7wage);
+console.log("wage = " + task_8wage);
+console.log("wage = " + task_9wage);
+console.log("wage = " + task_10wage);
 
 console.log("totalwage is: " + totalwage);
 console.log("totalwageround is: " + totalwageround);
 
 $("#result, #bottomform, #occupation, #td_bls, #td_blsrate").show();
 $("#task_1hour").text(task_1hour);
-$("#drivinghour").text(drivinghour);
-$("#homeworkhour").text(homeworkhour);
-$("#kidshour").text(kidshour);
-$("#cleaninghour").text(cleaninghour);
-$("#planninghour").text(planninghour);
-$("#shoppinghour").text(shoppinghour);
-$("#financeshour").text(financeshour);
-$("#yardhour").text(yardhour);
-$("#findinghour").text(findinghour);
+$("#task_2hour").text(task_2hour);
+$("#task_3hour").text(task_3hour);
+$("#task_4hour").text(task_4hour);
+$("#task_5hour").text(task_5hour);
+$("#task_6hour").text(task_6hour);
+$("#task_7hour").text(task_7hour);
+$("#task_8hour").text(task_8hour);
+$("#task_9hour").text(task_9hour);
+$("#task_10hour").text(task_10hour);
 $("#totalnumber").text(totalwageround);
 $("#occupation").text("general manager");
 $("#button").hide();
