bd1c30f23bf17e2d8a63414a00a507991e49a94b
Kuang Keng Kuek Ser <kuangkeng@gmail.com>
2015-05-09 01:32:40 -0400

    all tasks changed[B

--- script3.js
+++ script3.js
@@ -158,8 +158,8 @@ $("#button").hide();
 
 //code for dynamic twitter message
 
-var twittermsg ="Happy Mother’s Day to me! I should be making $"+totalwageround+" a year, or as much as a "+occupation+", for all the work I do. Calculate yours!";
-var twitterurl ='https://twitter.com/share?url=http://www.ibtimes.com/&text='+twittermsg+'&via=Ibtimes';
+var twittermsg ="Happy Mother’s Day to me! I should be making $"+totalwageround+" a year for all the work I do. Calculate yours!";
+var twitterurl ='https://twitter.com/share?url=http://www.ibtimes.com/&text='+twittermsg+'&via=IBTimes';
 console.log("twittermsg: " + twittermsg);
 console.log("twitterurl: " + twitterurl);
 
